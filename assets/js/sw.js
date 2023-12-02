0319c31f30af8870782604ce38eed0b1052cf445
Iefan <66840141+realiefan@users.noreply.github.com>
2023-12-02 05:44:02 +0530

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,18 +1,35 @@
-const CACHE = "NostrNet-V0.3";
+const CACHE_NAME = "NostrNet-V0.2";
 
-// Import workbox
+// Import workbox modules
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js"
 );
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-backgroundSync.js"
+);
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-push.js"
+);
 
 // Define caching strategies
-const staticCacheName = "static-v1.2";
+const staticCacheName = "static-v2";
+const dataSyncQueue = new workbox.backgroundSync.Queue("dataSyncQueue");
+
+// Precache static assets
+workbox.precaching.precacheAndRoute([
+  { url: "/", revision: "1" },
+  { url: "/index.html", revision: "1" },
+  { url: "/app.css", revision: "1" },
+  { url: "/assets/js/lists.js", revision: "1" },
+  // Additional assets...
+]);
+
+// Cache dynamic resources with stale-while-revalidate strategy
 workbox.routing.registerRoute(
-  new RegExp(".*(js|html|css)"),
+  new RegExp(".*(js|html|css|png|jpg|gif|woff2|json)"),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: staticCacheName,
     plugins: [
-      // Clear old cache entries on update
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 50,
         maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
@@ -21,20 +38,37 @@ workbox.routing.registerRoute(
   })
 );
 
-// Precache static assets
-workbox.precaching.precacheAndRoute([
-  { url: "/", revision: "1" },
-  { url: "/index.html", revision: "1" },
-  { url: "/app.css", revision: "1" },
-  { url: "/assets/js/lists.js", revision: "1" },
-]);
+// Background sync for data updates
+workbox.routing.registerRoute(
+  "/api/sync",
+  new workbox.backgroundSync.Plugin({
+    queue: dataSyncQueue,
+  })
+);
 
-// Fallback to network if cache fails
+// Push notifications handling
+workbox.push.register({
+  onPush: (event) => {
+    // Store notification data in IndexedDB
+    // ...
+
+    if (!navigator.onLine) {
+      return;
+    }
+
+    // Display the notification
+    // ...
+  },
+});
+
+// Offline fallback
 workbox.routing.setCatchHandler(async ({ event }) => {
   try {
-    // Fetch from network
-    const response = await fetch(event.request);
-    return response;
+    // Attempt to fetch from cache
+    const response = await caches.match(event.request);
+    if (response) {
+      return response;
+    }
   } catch (error) {
     // Offline page
     return new Response("Offline", {
@@ -51,7 +85,7 @@ self.addEventListener("message", (event) => {
   }
 });
 
-// Activate service worker and update cache
+// Activate service worker and clean up outdated caches
 self.addEventListener("activate", (event) => {
   self.clients.claim();
   event.waitUntil(workbox.precaching.cleanupOutdatedCaches());
