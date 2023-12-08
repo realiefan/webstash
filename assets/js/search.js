af419654b443d7bf8ad0bc86cfbf54b9e8d1a412
Iefan <66840141+realiefan@users.noreply.github.com>
2023-12-09 04:46:57 +0530

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,44 +1,13 @@
-const openCustomBrowser = (url) => {
-  // Create a new window
-  const inAppBrowser = window.open(url, "_self");
-
-  // Set custom styles
-  inAppBrowser.document.body.style.backgroundColor = "#f1f1f1";
-  inAppBrowser.document.body.style.color = "#222";
-  inAppBrowser.document.body.style.fontFamily = "sans-serif";
-
-  // Add custom buttons
-  const shareButton = inAppBrowser.document.createElement("button");
-  shareButton.textContent = "Share";
-  shareButton.addEventListener("click", () => {
-    // Implement sharing functionality
-  });
-  inAppBrowser.document.body.appendChild(shareButton);
-
-  // Disable context menu
-  inAppBrowser.document.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-  });
-
-  // Handle back button press
-  inAppBrowser.addEventListener("popstate", () => {
-    // Implement custom back button behavior
-  });
-};
-
 function performGoogleSearch() {
-  var searchTerm = document.getElementById("googleSearch").value;
+   var searchTerm = document.getElementById("googleSearch").value;
+   // Add your logic to perform the Google search with the searchTerm
+   // For example, you can redirect the user to the Google search results page
+   window.location.href =
+     "https://www.google.com/search?q=" + encodeURIComponent(searchTerm);
+ }
 
-  // Open a custom browser with Google search results
-  openCustomBrowser(
-    "https://www.google.com/search?q=" + encodeURIComponent(searchTerm)
-  );
-}
-
-document
-  .getElementById("googleSearch")
-  .addEventListener("keyup", function (event) {
+document.getElementById("googleSearch").addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
-      performGoogleSearch();
+        performGoogleSearch();
     }
-  });
+});
