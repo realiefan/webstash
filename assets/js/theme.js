d555b2cdc7e17db9fbde770344c13adbefcc06aa
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-06 12:58:42 +0000

    main

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,4 @@
 document.getElementById("themeButton").addEventListener("click", function () {
   // Replace "your-link-here" with the actual link you want to open
-  window.location.href = "https://bard.google.com/";
+  window.location.href = "https://labs.perplexity.ai/";
 });
