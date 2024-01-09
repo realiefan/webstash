0c806dbcfc444099d91dfcc7f47c769563c99c5a
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-09 16:27:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-// Import the Workbox library
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-// Set up Workbox with stale-while-revalidate strategy for static assets
 workbox.routing.registerRoute(
   /\.(css|js|png|jpg|jpeg|gif|svg|ico)$/,
   new workbox.strategies.StaleWhileRevalidate({
@@ -9,19 +7,25 @@ workbox.routing.registerRoute(
   })
 );
 
-// Precache specific assets during installation
+// Add a new route for the Icon Horse API
+workbox.routing.registerRoute(
+  /https:\/\/icon\.horse\/icon\//,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'icon-cache',
+  })
+);
+
 workbox.precaching.precacheAndRoute([
   { url: '/index.html', revision: '123456' },
   // Add other assets you want to precache
 ]);
 
-// Delete all previous caches during activation
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName.startsWith('NostrNet') && cacheName !== 'NostrNet-static-assets') {
+          if (cacheName.startsWith('NostrNet') && cacheName !== 'NostrNet-static-assets' && cacheName !== 'IconHorse-api') {
             return caches.delete(cacheName);
           }
         })
