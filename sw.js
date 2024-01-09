ec8efff06f4b2e386ffb18db6e278541b00b6a35
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-09 16:38:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,93 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
+
+
+// Cache your HTML pages using a cache-first strategy with versioning
+
 workbox.routing.registerRoute(
-  /\.(css|js|png|jpg|jpeg|gif|svg|ico)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'NostrNet-static-assets',
+
+  /\.(html)$/,
+
+  new workbox.strategies.CacheFirst({
+
+    cacheName: 'NostrNet-html-pages-v1', // Update the cache name with versioning
+
   })
+
 );
 
-// Add a new route for the Icon Horse API
+
+
 workbox.routing.registerRoute(
-  /https:\/\/icon\.horse\/icon\//,
+
+  /\.(css|js|png|jpg|jpeg|gif|svg|ico)$/,
+
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'icon-cache',
+
+    cacheName: 'NostrNet-static-assets-v1', // Update the cache name with versioning
+
   })
+
 );
+
+
+
+workbox.routing.registerRoute(
+
+  /https:\/\/icon\.horse\/icon\//,
+
+  new workbox.strategies.StaleWhileRevalidate({
+
+    cacheName: 'IconHorse-api-v1', // Update the cache name with versioning
+
+  })
+
+);
+
+
 
 workbox.precaching.precacheAndRoute([
-  { url: '/index.html', revision: '123456' },
-  // Add other assets you want to precache
+
+  // Precache your HTML pages with versioning
+
+  { url: '/index.html', revision: 'v1' },
+
+  { url: '/assets/pages/wallet/wallet.html', revision: 'v1' },
+
+  { url: '/assets/pages/backup/backup.html', revision: 'v1' },
+
+  { url: '/assets/pages/control/control.html', revision: 'v1' },
+
+  // Update revision numbers for other assets
+
 ]);
 
+
+
 self.addEventListener('activate', (event) => {
+
   event.waitUntil(
+
     caches.keys().then((cacheNames) => {
+
       return Promise.all(
+
         cacheNames.map((cacheName) => {
-          if (cacheName.startsWith('NostrNet') && cacheName !== 'NostrNet-static-assets' && cacheName !== 'IconHorse-api') {
+
+          if (cacheName.startsWith('NostrNet') && !cacheName.includes('v1.1')) {
+
             return caches.delete(cacheName);
+
           }
+
         })
+
       );
+
     })
+
   );
+
 });
+
+
